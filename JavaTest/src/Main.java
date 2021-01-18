9dd418b62ec6caca9ab9d68b6fa0763eb307401c
Diego <nerydiego85@gmail.com>
2021-01-18 13:02:48 -0600

    Added Scanner for user name

--- JavaTest/src/Main.java
+++ JavaTest/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Git example!");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        String hello = "Hello World";
+        System.out.println(hello + " " + name);
     }
 }
